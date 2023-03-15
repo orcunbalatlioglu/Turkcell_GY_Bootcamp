d2662b21af6959396eb6b30f6c6dae7897fd1efa
Orçun Balatlıoğlu <orcun521@gmail.com>
2023-03-15 21:05:03 +0300

    2. Hafta 1. Derste verilen parola kontrol uygulaması ödevi.

---
+++ 2. Hafta Ödev/PasswordSecurityCheck/PasswordSecurityCheck/CharCheck.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasswordSecurityCheck
+{
+    public enum SecurityLevel
+    {
+        INVALID,
+        LOW,
+        MIDDLE,
+        HIGH
+    }
+    public class PasswordCheck
+    {
+        /// <summary>
+        /// Checks for numbers, letters and special characters in the input string
+        /// </summary>
+        /// <param name="PasswordInput"></param>
+        /// <returns> It returns an enumeric value which defined in SecurityLevel. It can be INVALID,LOW,MIDDLE,HIGH.</returns>
+        /// <exception cref="Exception">It is for invalid enumeric value. It can cause value miscalculation.</exception>
+        public SecurityLevel CheckPasswordSecurity(string PasswordInput)
+        {
+            int tempSecurityLevel = 0;
+
+            if (isPasswordValid(PasswordInput))
+            {
+                return SecurityLevel.INVALID;
+            }
+            else {
+                if (isThereNumber(PasswordInput))
+                {
+                    tempSecurityLevel++;
+                }
+                if (isThereLetter(PasswordInput))
+                {
+                    tempSecurityLevel++;
+                }
+                if (isThereSpecialCharacter(PasswordInput))
+                {
+                    tempSecurityLevel++;
+                } 
+
+                switch (tempSecurityLevel)
+                {
+                    case 1:
+                        return SecurityLevel.LOW;
+                        break;
+                    case 2:
+                        return SecurityLevel.MIDDLE;
+                        break;
+                    case 3:
+                        return SecurityLevel.HIGH;
+                        break;
+                    default:
+                        throw new Exception("Invalid enumeric value for SecurityLevel!");
+                }
+            }
+
+        }
+
+        bool isPasswordValid(string PasswordInput)
+        {
+            if (PasswordInput.Length < 6)
+                return true;
+            else
+                return false;
+        }
+
+        bool isThereNumber(string PasswordInput)
+        {
+            foreach (char c in PasswordInput)
+            {
+                if(char.IsNumber(c))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        bool isThereLetter(string PasswordInput)
+        {
+            foreach (char c in PasswordInput)
+            {
+                if (char.IsLetter(c))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        bool isThereSpecialCharacter(string PasswordInput)
+        {
+            foreach (char c in PasswordInput)
+            {
+                if (!char.IsLetterOrDigit(c))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
---
+++ 2. Hafta Ödev/PasswordSecurityCheck/PasswordSecurityCheck/Main.cs
@@ -0,0 +1,16 @@
+﻿using PasswordSecurityCheck;
+
+Console.WriteLine("Enter your password:");
+try { 
+    string UserPassword = Console.ReadLine();
+    PasswordCheck pswCheck = new PasswordCheck();
+    var output = pswCheck.CheckPasswordSecurity(UserPassword);
+    if (output != SecurityLevel.INVALID)
+        Console.WriteLine($"Your password has {output} security level!");
+    else
+        Console.WriteLine($"Your password is {output}. It must be least 6 character!");
+}
+catch(Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
