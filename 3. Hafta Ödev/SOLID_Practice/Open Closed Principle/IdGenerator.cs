d75027a95846a6d18f4839046047e85e3ec95faf
Orçun Balatlıoğlu <orcun521@gmail.com>
2023-03-29 20:03:31 +0300

    Daha önce srp için tasarlanan form uygulaması üzerinde open closed prensibi uygulaması

---
+++ 3. Hafta Ödev/SOLID_Practice/Open Closed Principle/IdGenerator.cs
@@ -0,0 +1,15 @@
+﻿namespace OpenClosedPrinciple
+{
+
+    public class IdGenerator
+    {
+        private static int currentId = 0;
+
+        public static int NextId()
+        {
+            currentId++;
+            return currentId;
+        }
+    }
+    
+}
