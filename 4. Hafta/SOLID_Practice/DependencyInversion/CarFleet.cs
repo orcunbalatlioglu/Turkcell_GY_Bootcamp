47a1b7379d525d82de722abcffabd54b383ae8ea
Orçun Balatlıoğlu <orcun521@gmail.com>
2023-03-31 20:48:45 +0300

    Dependency inversion practice with car fleet

---
+++ 4. Hafta/SOLID_Practice/DependencyInversion/CarFleet.cs
@@ -0,0 +1,15 @@
+﻿namespace DependencyInversion
+{
+    internal class CarFleet 
+    {
+        List<ICarType> Cars { get; set; }
+
+        public Fleet(List<ICarType> cars)
+        {
+            Cars = cars;
+        }
+
+        public void AddCar() { }
+        public void RemoveCar() { }
+    }
+}
---
+++ 4. Hafta/SOLID_Practice/DependencyInversion/ICarType.cs
@@ -0,0 +1,63 @@
+﻿namespace DependencyInversion
+{
+    internal interface ICarType
+    {
+        string Brand { get; set; }
+        string Model { get; set; }
+        string Description { get; set; }
+
+        void PrintAllInfo();
+    }
+
+    public class Fuel : ICarType
+    {
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public string Description { get; set; }
+        string FuelType { get; set; }
+        int FuelCapacity { get; set; }
+        int RemainingFuel { get; set; }
+
+        public Fuel() { }
+
+        public void PrintAllInfo()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class Electric : ICarType
+    {
+        public string Brand { get ; set ; }
+        public string Model { get ; set ; }
+        public string Description { get ; set; }
+        int BatteryCapacityKW { get; set; }
+        int RemainingBattery { get; set; }
+        
+        public Electric() { }
+
+        public void PrintAllInfo()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class Hybrid : ICarType
+    {
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public string Description { get; set; }
+        int BatteryCapacityKW { get; set; }
+        int RemainingBattery { get; set; }
+        string FuelType { get; set; }
+        int FuelCapacity { get; set; }
+        int RemainingFuel { get; set; }
+
+        public Hybrid() { }
+
+        public void PrintAllInfo()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
